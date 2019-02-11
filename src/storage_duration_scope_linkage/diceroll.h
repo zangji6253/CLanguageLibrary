9c3b4a135aff1ebe4f720108ef43b42bd898ec3c
Jian Zhang <184263567@qq.com>
2019-02-11 09:35:42 +0800

    1

---
+++ src/storage_duration_scope_linkage/diceroll.h
@@ -0,0 +1,14 @@
+//
+// Created by j on 19-1-23.
+//
+
+#ifndef CLANGUAGELIBRARY_DICEROLL_H
+#define CLANGUAGELIBRARY_DICEROLL_H
+
+#endif //CLANGUAGELIBRARY_DICEROLL_H
+
+//diceroll
+
+extern int roll_count;
+
+int roll_n_dice(int dice, int sides);
